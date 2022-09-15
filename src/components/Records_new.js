6c9d33910053b2a64024a8a424f0230a94f614a8
Nathanon58 <nathanon58@gmail.com>
2022-09-15 21:59:52 +0700

    feat: 🎸 record
    add new button

--- src/components/Records_new.js
+++ src/components/Records_new.js
@@ -3,6 +3,7 @@ import {Button} from 'antd';
 const Records_new = () => {
     return(
         <div style={{textAlign: "center"}}>
+            New Record
             <Button>ลูกค้าเก่า</Button><br/>
             <Button>ลูกค้าใหม่</Button><br/>
         </div>
