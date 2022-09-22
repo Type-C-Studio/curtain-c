11ba4a36dc44bb51be6f9d1ac6d43a254a98811d
oasiss1123 <tuey-22@hotmail.com>
2022-09-22 22:24:23 +0700

    feat: 🎸 80% new records form
    almost complete new records form

--- src/components/Records_new.js
+++ src/components/Records_new.js
@@ -1,4 +1,13 @@
-import { Button, Input, Divider, Select, Space, Form } from "antd";
+import {
+  Button,
+  Input,
+  Divider,
+  Select,
+  Space,
+  Form,
+  DatePicker,
+  Checkbox,
+} from "antd";
 import { Container, Card } from "react-bootstrap";
 import React, { useState, useRef } from "react";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
@@ -26,172 +35,180 @@ const Records_new = () => {
     console.log("Received values of form:", values);
   };
 
-  const onFinish2 = (values) => {
-    console.log("Received values of form:", values);
-  };
+  const [componentDisabled1, setComponentDisabled1] = useState(true);
+
+  const [componentDisabled, setComponentDisabled] = useState(true);
+
   return (
     <div>
-     
       <Container>
         <Form
           name="dynamic_form_nest_item"
           onFinish={onFinish}
           autoComplete="off"
         >
-          
-
-           <div  style={{textAlign: "left"}}>
-           <h3>Customer</h3>
+          <div style={{ textAlign: "left" }}>
+            <h3>Customer</h3>
             <label>Name</label>
-            <Form.Item name="customer_name" >
-              <Input placeholder="name" className="form-control mt-1"/>
+            <Form.Item name="customer_name">
+              <Input placeholder="name" className="form-control mt-1" />
             </Form.Item>
 
             <label>Address</label>
-            <Form.Item name="customer_address" >
-              <Input placeholder="address" className="form-control mt-1"/>
+            <Form.Item name="customer_address">
+              <Input placeholder="address" className="form-control mt-1" />
             </Form.Item>
 
             <label>Tel</label>
-            <Form.Item name="customer_tel" >
-              <Input placeholder="tel" className="form-control mt-1"/>
+            <Form.Item name="customer_tel">
+              <Input placeholder="tel" className="form-control mt-1" />
             </Form.Item>
 
             <label>Email</label>
-            <Form.Item name="customer_email" >
-              <Input placeholder="email" className="form-control mt-1"/>
+            <Form.Item name="customer_email">
+              <Input placeholder="email" className="form-control mt-1" />
             </Form.Item>
 
             <label>Company</label>
-            <Form.Item name="customer_company" >
-              <Input placeholder="company" className="form-control mt-1"/>
+            <Form.Item name="customer_company">
+              <Input placeholder="company" className="form-control mt-1" />
             </Form.Item>
 
             <label>Tax</label>
-            <Form.Item name="customer_tax" >
-              <Input placeholder="tax" className="form-control mt-1"/>
+            <Form.Item name="customer_tax">
+              <Input placeholder="tax" className="form-control mt-1" />
             </Form.Item>
 
+            <label>Date</label>
+            <Form.Item name="date">
+              <DatePicker
+                placeholder="Select Date"
+                className="form-control mt-1"
+              />
+            </Form.Item>
+
+            <h3>Rooms</h3>
+            <Form.List name="rooms">
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map(({ key, name, ...restField }) => (
+                    <>
+                      <label>Room Name</label>
+                      <Form.Item key={key} name={[name, "room"]}>
+                        <Input
+                          placeholder="room"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ม่าน</label>
+                      <Form.Item name={[name, "name"]}>
+                        <Input
+                          placeholder="ม่าน"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ราคา</label>
+                      <Form.Item name={[name, "price"]}>
+                        <Input
+                          placeholder="ราคา"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>กว้าง</label>
+                      <Form.Item name={[name, "width"]}>
+                        <Input
+                          placeholder="Width"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>สูง</label>
+                      <Form.Item name={[name, "height"]}>
+                        <Input
+                          placeholder="Height"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ล็อคลอน</label>
+                      <Form.Item name={[name, "pattern"]}>
+                        <Input
+                          placeholder="ล็อคลอน"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>หน้าผ้า</label>
+                      <Form.Item name={[name, "frontSpace"]}>
+                        <Input
+                          placeholder="หน้าผ้า"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ราคาราง</label>
+                      <Form.Item name={[name, "rail_price"]}>
+                        <Input
+                          placeholder="ราคาราง"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <Checkbox
+                        checked={componentDisabled}
+                        onChange={(e) => setComponentDisabled(e.target.checked)}
+                      >
+                        <label>ผ้าโปร่ง</label>
+                      </Checkbox>
+
+                      <Form.Item name={[name, "ocpacity_curtain_price"]}>
+                        <Input
+                          disabled={!componentDisabled}
+                          placeholder="ราคาผ้าโปร่ง"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <Checkbox
+                        checked={componentDisabled1}
+                        onChange={(e) =>
+                          setComponentDisabled1(e.target.checked)
+                        }
+                      >
+                        <label>ผ้าทึบ</label>
+                      </Checkbox>
+
+                      <Form.Item name={[name, "dark_curtain_price"]}>
+                        <Input
+                          disabled={!componentDisabled1}
+                          placeholder="ราคาผ้าทึบ"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+                    </>
+                  ))}
+                  <Button
+                    className="color-nav"
+                    onClick={() => add()}
+                    icon={<PlusOutlined />}
+                  >
+                    Room
+                  </Button>
+                </>
+              )}
+            </Form.List>
           </div>
 
-          
-
-
-          <Form.List name="rooms">
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map(({ key, name, ...restField }) => (
-                  <Space
-                    key={key}
-                    style={{
-                      display: "flex",
-                      marginBottom: 8,
-                    }}
-                    align="baseline"
-                  >
-                    <Card style={{ width: "18rem" }}>
-                      <Card.Body>
-                        <Card.Title>
-                          <Form.Item {...restField} name={[name, "room"]}>
-                            <Input
-                              placeholder="Room"
-                              style={{
-                                width: "60%",
-                              }}
-                            />
-                          </Form.Item>
-                        </Card.Title>
-
-                        <Form.List name="details">
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map(({ key, name, ...restField }) => (
-                  <Space
-                    key={key}
-                    style={{
-                      display: "flex",
-                      marginBottom: 8,
-                    }}
-                    align="baseline"
-                  >
-                    <Card style={{ width: "18rem" }}>
-                      <Card.Body>
-                        <Card.Title>
-                          <Form.Item {...restField} name={[name, "detail"]}>
-                            <Input
-                              placeholder="Room"
-                              style={{
-                                width: "60%",
-                              }}
-                            />
-                          </Form.Item>
-                        </Card.Title>
-                        <Card.Footer>
-                          <Button
-                            onClick={() => remove(name)}
-                            style={{ float: "right" }}
-                          >
-                            Remove room
-                          </Button>
-                        </Card.Footer>
-                      </Card.Body>
-                    </Card>
-                  </Space>
-                ))}
-
-                <Button
-                  className="color-nav"
-                  onClick={() => add()}
-                  icon={<PlusOutlined />}
-                >
-                  Room
-                </Button>
-              </>
-            )}
-          </Form.List>
-
-
-
-
-
-
-
-
-                        <Card.Footer>
-                          <Button
-                            onClick={() => remove(name)}
-                            style={{ float: "right" }}
-                          >
-                            Remove room
-                          </Button>
-                        </Card.Footer>
-                      </Card.Body>
-                    </Card>
-                  </Space>
-                ))}
-
-                <Button
-                  className="color-nav"
-                  onClick={() => add()}
-                  icon={<PlusOutlined />}
-                >
-                  Room
-                </Button>
-              </>
-            )}
-          </Form.List>
-
-
-          
           <Form.Item>
             <Button type="primary" htmlType="submit">
               Overview
             </Button>
           </Form.Item>
         </Form>
-      
       </Container>
-      
     </div>
   );
 };
