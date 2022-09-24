c885d542318895d430a9dcf251bae36f16816754
oasiss1123 <tuey-22@hotmail.com>
2022-09-24 13:50:46 +0700

    feat: 🎸 make records edit page
    make records edit page but can't connect api

--- src/components/RecordEdit.js
+++ src/components/RecordEdit.js
@@ -1,8 +1,215 @@
+import {
+  Button,
+  Input,
+  Divider,
+  Select,
+  Space,
+  Form,
+  DatePicker,
+  Checkbox,
+} from "antd";
+import { Container, Card } from "react-bootstrap";
+import React, { useState, useRef, useEffect } from "react";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const { Option } = Select;
+
 const RecordEdit = () => {
-    return(
-        <div>
-            record Ei Ei
-        </div>
-    )
-}
+  const onFinish = (values) => {
+    console.log("Received values of form:", values);
+  };
+
+  const [componentDisabled1, setComponentDisabled1] = useState(true);
+
+  const [componentDisabled, setComponentDisabled] = useState(true);
+
+  const { id } = useParams();
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get(`http://rhome19.thddns.net:5524/api/job/${id}`).then((res) => {
+      // not finish yet
+      res.data.map((items) => {
+        setData(items);
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Edit</h1>
+      <Container>
+        <Form
+          name="dynamic_form_nest_item"
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <div style={{ textAlign: "left" }}>
+            <h3>Customer </h3>
+            <label>Name</label>
+            <Form.Item name="customer_name">
+              <Input placeholder="name" className="form-control mt-1" />
+            </Form.Item>
+
+            <label>Address</label>
+            <Form.Item name="customer_address">
+              <Input placeholder="address" className="form-control mt-1" />
+            </Form.Item>
+
+            <label>Tel</label>
+            <Form.Item name="customer_tel">
+              <Input placeholder="tel" className="form-control mt-1" />
+            </Form.Item>
+
+            <label>Email</label>
+            <Form.Item name="customer_email">
+              <Input placeholder="email" className="form-control mt-1" />
+            </Form.Item>
+
+            <label>Company</label>
+            <Form.Item name="customer_company">
+              <Input placeholder="company" className="form-control mt-1" />
+            </Form.Item>
+
+            <label>Tax</label>
+            <Form.Item name="customer_tax">
+              <Input placeholder="tax" className="form-control mt-1" />
+            </Form.Item>
+
+            <label>Date</label>
+            <Form.Item name="date">
+              <DatePicker
+                placeholder="Select Date"
+                className="form-control mt-1"
+              />
+            </Form.Item>
+
+            <h3>Rooms</h3>
+            <Form.List name="rooms">
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map(({ key, name, ...restField }) => (
+                    <>
+                      <label>Room Name</label>
+                      <Form.Item key={key} name={[name, "room"]}>
+                        <Input
+                          placeholder="room"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ม่าน</label>
+                      <Form.Item name={[name, "name"]}>
+                        <Input
+                          placeholder="ม่าน"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ราคา</label>
+                      <Form.Item name={[name, "price"]}>
+                        <Input
+                          placeholder="ราคา"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>กว้าง</label>
+                      <Form.Item name={[name, "width"]}>
+                        <Input
+                          placeholder="Width"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>สูง</label>
+                      <Form.Item name={[name, "height"]}>
+                        <Input
+                          placeholder="Height"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ล็อคลอน</label>
+                      <Form.Item name={[name, "pattern"]}>
+                        <Input
+                          placeholder="ล็อคลอน"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>หน้าผ้า</label>
+                      <Form.Item name={[name, "frontSpace"]}>
+                        <Input
+                          placeholder="หน้าผ้า"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <label>ราคาราง</label>
+                      <Form.Item name={[name, "rail_price"]}>
+                        <Input
+                          placeholder="ราคาราง"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <Checkbox
+                        checked={componentDisabled}
+                        onChange={(e) => setComponentDisabled(e.target.checked)}
+                      >
+                        <label>ผ้าโปร่ง</label>
+                      </Checkbox>
+
+                      <Form.Item name={[name, "ocpacity_curtain_price"]}>
+                        <Input
+                          disabled={!componentDisabled}
+                          placeholder="ราคาผ้าโปร่ง"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+
+                      <Checkbox
+                        checked={componentDisabled1}
+                        onChange={(e) =>
+                          setComponentDisabled1(e.target.checked)
+                        }
+                      >
+                        <label>ผ้าทึบ</label>
+                      </Checkbox>
+
+                      <Form.Item name={[name, "dark_curtain_price"]}>
+                        <Input
+                          disabled={!componentDisabled1}
+                          placeholder="ราคาผ้าทึบ"
+                          className="form-control mt-1"
+                        />
+                      </Form.Item>
+                    </>
+                  ))}
+                  <Button
+                    className="color-nav"
+                    onClick={() => add()}
+                    icon={<PlusOutlined />}
+                  >
+                    Room
+                  </Button>
+                </>
+              )}
+            </Form.List>
+          </div>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Overview
+            </Button>
+          </Form.Item>
+        </Form>
+      </Container>
+    </div>
+  );
+};
 export default RecordEdit;
--- src/components/Records.js
+++ src/components/Records.js
@@ -1,106 +1,107 @@
-import {Navbar, Container} from 'react-bootstrap';
-import mainLogo from'../photos/Mantarin_haus_logo.png';
-import { Divider, Radio, Table, Button} from 'antd';
-import React, { useState } from 'react';
+import { Navbar, Container } from "react-bootstrap";
+import mainLogo from "../photos/Mantarin_haus_logo.png";
+import { Divider, Radio, Table, Button } from "antd";
+import React, { useState } from "react";
 import { FormOutlined } from "@ant-design/icons";
 
-
 const Records = () => {
-    const [id, setId] = useState();
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          render: (text) => <a href="#">{text}</a>,
-        },
-        {
-          title: 'Date',
-          dataIndex: 'date',
-          sorter: (a, b) => a.date - b.date
-        },
-        {
-          title: 'Status',
-          dataIndex: 'status',
-          render: (text) => text === "Success" ? ( <div
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text) => <a href="#">{text}</a>,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+      sorter: (a, b) => a.date - b.date,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (text) =>
+        text === "Success" ? (
+          <div
             style={{
-                backgroundColor: "#8cfac1",
-                color: "#1d8e33",
-                textAlign: "center",
-                borderRadius: 15,
+              backgroundColor: "#8cfac1",
+              color: "#1d8e33",
+              textAlign: "center",
+              borderRadius: 15,
             }}
           >
-            {text} 
-          </div>) : text === "Processing" ? (
-              <div
-              style={{
-                backgroundColor: "#ffc471",
-                color: "#a76e10",
-                textAlign: "center",
-                borderRadius: 15,
-              }}
-            >
-              {text}
-            </div>) : (<div
-              style={{
-                backgroundColor: "#dbdbdb",
-                color: "#6e6e6e",
-                textAlign: "center",
-                borderRadius: 15,
-              }}
-            >
-              {text}
-            </div>)
-        },
-        {
-            render: (rec) => (<Button
-                style={{ color: "#286efb" }}
-               
-                icon={<FormOutlined />}
-                type="link"
-               
-              ></Button>)
-        }
-      ];
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          date: "20/09/2022",
-          status: 'Canceled',
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          date: "20/09/2021",
-          status: 'Processing',
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          date: "18/09/2022",
-          status: 'Success',
-        },
-        {
-          key: '4',
-          name: 'Disabled User',
-          date: "22/08/2022",
-          status: 'Success',
-        },
-      ]; // rowSelection object indicates the need for row selection
-      
-     
-       
-      
-    
+            {text}
+          </div>
+        ) : text === "Processing" ? (
+          <div
+            style={{
+              backgroundColor: "#ffc471",
+              color: "#a76e10",
+              textAlign: "center",
+              borderRadius: 15,
+            }}
+          >
+            {text}
+          </div>
+        ) : (
+          <div
+            style={{
+              backgroundColor: "#dbdbdb",
+              color: "#6e6e6e",
+              textAlign: "center",
+              borderRadius: 15,
+            }}
+          >
+            {text}
+          </div>
+        ),
+    },
+    {
+      render: (rec, gg) => (
+        <Button
+          style={{ color: "#286efb" }}
+          icon={<FormOutlined />}
+          type="link"
+          href={`/record/edit/${rec.id}`}
+        ></Button>
+      ),
+    },
+  ];
+  const data = [
+    {
+      key: "1",
+      id: "1",
+      name: "John Brown",
+      date: "20/09/2022",
+      status: "Canceled",
+    },
+    {
+      key: "2",
+      id: "2",
+      name: "Jim Green",
+      date: "20/09/2021",
+      status: "Processing",
+    },
+    {
+      key: "3",
+      id: "3",
+      name: "Joe Black",
+      date: "18/09/2022",
+      status: "Success",
+    },
+    {
+      key: "4",
+      id: "4",
+      name: "Disabled User",
+      date: "22/08/2022",
+      status: "Success",
+    },
+  ]; // rowSelection object indicates the need for row selection
 
-return(
-    <div style={{textAlign: "center"}}>
-         
+  return (
+    <div style={{ textAlign: "center" }}>
       <div>
-     
-
-      <Divider />
-      <Button
+        <Divider />
+        <Button
           style={{
             fontWeight: "bold",
             float: "right",
@@ -116,17 +117,9 @@ return(
         >
           New
         </Button>
-      <Table
-      
-        columns={columns}
-        dataSource={data}
-      
-      />
+        <Table columns={columns} dataSource={data} />
+      </div>
     </div>
-
-  
-      
-    </div>
-)
-}
+  );
+};
 export default Records;
